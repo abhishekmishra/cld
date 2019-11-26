cf586204522a0ae07cecdd51d0aa83730d629d3e
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-11-26 19:09:34 +0530

    created coll build in docker

--- docker_build/debian/build.sh
+++ docker_build/debian/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd /
+
 SW_HOME="/sw"
-VCPKG_HOME="${SW_HOME}/vcpkg"
+export VCPKG_HOME="${SW_HOME}/vcpkg"
+CODE_HOME="/code"
 
 # vcpkg installation
 if [ -d "${VCPKG_HOME}" ]; then
@@ -21,3 +24,26 @@ ${VCPKG_HOME}/vcpkg install curl
 ${VCPKG_HOME}/vcpkg install libarchive
 ${VCPKG_HOME}/vcpkg install cmocka
 ${VCPKG_HOME}/vcpkg install json-c
+${VCPKG_HOME}/vcpkg install lua
+
+echo "VCPKG packages installed."
+
+if [ -d "$CODE_HOME/coll" ]; then
+    echo "coll is already cloned."
+    cd "$CODE_HOME/coll"
+    git pull
+    cd -
+else
+    cd "$CODE_HOME"
+    git clone https://github.com/abhishekmishra/coll.git
+    cd -
+fi
+
+if [ -d "$CODE_HOME/coll" ]; then
+    echo "Start coll build"
+    cd "$CODE_HOME/coll"
+    rm -fR ./build
+    ./build_linux.sh
+    ./build/coll_test
+    cd -
+fi
