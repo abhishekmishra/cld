995ae517360c75830d8ac9239cc60a8bcd41cdfc
Abhishek Mishra <abhishekmishra3@gmail.com>
2020-04-01 15:22:00 +0530

    container ls initial impl in lua

--- lua/cmd_container.lua
+++ lua/cmd_container.lua
@@ -2,10 +2,39 @@ json = require("json")
 docker = require("luaclibdocker")
 
 function cmd_ctr_ls(d, options, args)
-    ctr_ls_str = d:container_ls()
-    ctr_ls = json.decode(ctr_ls_str)
-    print(ctr_ls)
-    for k, v in ipairs(ctr_ls) do
-        print("Container #" .. k .. " is " .. v.Names[1])
+    all = false
+    limit = 0
+    size = true
+
+    if options ~= nil then
+        if options["--all"] ~= nil then
+            all = options["--all"].val
+        end
     end
+
+    ctr_ls_str = d:container_ls(all, limit, size)
+    ctr_ls = json.decode(ctr_ls_str)
+    -- print(ctr_ls_str)
+
+    output = {}
+    for k, v in ipairs(ctr_ls) do
+        ctr = {}
+        ctr["Command"] = v.Command
+        ctr["CreatedAt"] = v.Created
+        ctr["ID"] = v.Id
+        ctr["Image"] = v.Image
+        ctr["Labels"] = v.Labels
+        ctr["LocalVolumes"] = v.Mounts
+        ctr["Mounts"] = v.Mounts
+        ctr["Names"] = v.Names
+        ctr["Networks"] = v.NetworkSettings.Networks
+        ctr["Ports"] = v.Ports
+        ctr["RunningFor"] = nil
+        ctr["Size"] = v.SizeRootFs
+        ctr["Status"] = v.Status
+
+        table.insert(output, ctr)
+        -- print("Container #" .. k .. " is " .. v.Names[1])
+    end
+    return output
 end
--- lua/test/main.lua
+++ lua/test/main.lua
@@ -4,4 +4,38 @@ cmd_container = require("cmd_container")
 
 d = docker.connect()
 
-cmd_ctr_ls(d)
+ls_options = {
+    ["--all"] = {
+        ["val"] = false,
+    },
+    ["--filter"] = {
+        ["val"] = nil,
+    },
+    ["--format"] = {
+        ["val"] = nil,
+    },
+    ["--last"] = {
+        ["val"] = nil,
+    },
+    ["--latest"] = {
+        ["val"] = false,
+    },
+    ["--no-trunc"] = {
+        ["val"] = false,
+    },
+    ["--quiet"] = {
+        ["val"] = false,
+    },
+    ["--size"] = {
+        ["val"] = 0,
+    },
+}
+output = cmd_ctr_ls(d, ls_options)
+for k, v in ipairs(output) do
+    print("Container #" .. k)
+    for kt, vt in pairs(v) do
+        print("\t" .. kt .. " = ")
+        print(vt)
+    end
+    print()
+end
