514fabb51acbd98a34afc3da275e947b40a175ba
Abhishek Mishra <abhishekmishra3@gmail.com>
2024-09-16 23:12:40 +0530

    tabs to spaces

--- src/cld.c
+++ src/cld.c
@@ -94,187 +94,187 @@ static int loglevel = LOG_ERROR;
 
 void docker_result_handler(docker_context *ctx, docker_result *res)
 {
-	handle_docker_error(res, (zclk_command_output_handler)&print_handler, (zclk_command_output_handler)&print_handler);
+    handle_docker_error(res, (zclk_command_output_handler)&print_handler, (zclk_command_output_handler)&print_handler);
 }
 
 zclk_res main_cmd_handler(zclk_command* cmd, void *handler_args)
 {
-	zclk_option *debug_option = get_option_by_name(cmd->options, ZCLK_OPTION_MAIN_LOG_LEVEL_LONG);
-	if (zclk_option_get_val_string(debug_option) != NULL)
-	{
-		if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_DEBUG) == 0)
-		{
-			docker_log_set_level(LOG_DEBUG);
-			loglevel = LOG_DEBUG;
-		}
-		if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_INFO) == 0)
-		{
-			docker_log_set_level(LOG_INFO);
-			loglevel = LOG_INFO;
-		}
-		if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_WARN) == 0)
-		{
-			docker_log_set_level(LOG_WARN);
-			loglevel = LOG_WARN;
-		}
-		if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_ERROR) == 0)
-		{
-			docker_log_set_level(LOG_ERROR);
-			loglevel = LOG_ERROR;
-		}
-		if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_FATAL) == 0)
-		{
-			docker_log_set_level(LOG_FATAL);
-			loglevel = LOG_FATAL;
-		}
-	}
+    zclk_option *debug_option = get_option_by_name(cmd->options, ZCLK_OPTION_MAIN_LOG_LEVEL_LONG);
+    if (zclk_option_get_val_string(debug_option) != NULL)
+    {
+        if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_DEBUG) == 0)
+        {
+            docker_log_set_level(LOG_DEBUG);
+            loglevel = LOG_DEBUG;
+        }
+        if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_INFO) == 0)
+        {
+            docker_log_set_level(LOG_INFO);
+            loglevel = LOG_INFO;
+        }
+        if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_WARN) == 0)
+        {
+            docker_log_set_level(LOG_WARN);
+            loglevel = LOG_WARN;
+        }
+        if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_ERROR) == 0)
+        {
+            docker_log_set_level(LOG_ERROR);
+            loglevel = LOG_ERROR;
+        }
+        if (strcmp(zclk_option_get_val_string(debug_option), CLD_LOGLEVEL_FATAL) == 0)
+        {
+            docker_log_set_level(LOG_FATAL);
+            loglevel = LOG_FATAL;
+        }
+    }
 
-	zclk_option *host_option = get_option_by_name(cmd->options, ZCLK_OPTION_MAIN_HOST_LONG);
+    zclk_option *host_option = get_option_by_name(cmd->options, ZCLK_OPTION_MAIN_HOST_LONG);
 
-	if (!connected)
-	{
-		char *url;
+    if (!connected)
+    {
+        char *url;
 
-		if (zclk_option_get_val_string(host_option) == NULL)
-		{
-			{
-				if (make_docker_context_default_local(&ctx) == E_SUCCESS)
-				{
-					url = str_clone(ctx->url);
-					connected = 1;
-				}
-			}
-		}
-		else
-		{
-			url = zclk_option_get_val_string(host_option);
-			if (is_http_url(url))
-			{
-				if (make_docker_context_url(&ctx, url) == E_SUCCESS)
-				{
-					connected = true;
-				}
-			}
-			else if (is_unix_socket(url))
-			{
-				if (make_docker_context_url(&ctx, url) == E_SUCCESS)
-				{
-					connected = true;
-				}
-			}
-		}
+        if (zclk_option_get_val_string(host_option) == NULL)
+        {
+            {
+                if (make_docker_context_default_local(&ctx) == E_SUCCESS)
+                {
+                    url = str_clone(ctx->url);
+                    connected = 1;
+                }
+            }
+        }
+        else
+        {
+            url = zclk_option_get_val_string(host_option);
+            if (is_http_url(url))
+            {
+                if (make_docker_context_url(&ctx, url) == E_SUCCESS)
+                {
+                    connected = true;
+                }
+            }
+            else if (is_unix_socket(url))
+            {
+                if (make_docker_context_url(&ctx, url) == E_SUCCESS)
+                {
+                    connected = true;
+                }
+            }
+        }
 
-		if (connected)
-		{
-			lua_set_docker_context(ctx, loglevel);
-			docker_context_result_handler_set(ctx, (docker_result_handler_fn *)&docker_result_handler);
-			// if (docker_ping(ctx) != E_SUCCESS)
-			//{
-			//	docker_log_fatal("Could not ping the server %s", url);
-			//	connected = 0;
-			// }
-			// else
-			//{
-			//	docker_log_debug("%s is alive.", url);
-			// }
-		}
-	}
+        if (connected)
+        {
+            lua_set_docker_context(ctx, loglevel);
+            docker_context_result_handler_set(ctx, (docker_result_handler_fn *)&docker_result_handler);
+            // if (docker_ping(ctx) != E_SUCCESS)
+            //{
+            //    docker_log_fatal("Could not ping the server %s", url);
+            //    connected = 0;
+            // }
+            // else
+            //{
+            //    docker_log_debug("%s is alive.", url);
+            // }
+        }
+    }
 
-	if (!connected)
-	{
-		exit(-1);
-	}
+    if (!connected)
+    {
+        exit(-1);
+    }
 
-	return ZCLK_RES_SUCCESS;
+    return ZCLK_RES_SUCCESS;
 }
 
 zclk_command *create_main_command()
 {
-	zclk_command *main_command = new_zclk_command(main_command_name, "cld",
-												 "CLD Docker Client",
-												 &main_cmd_handler);
-	
-	if (main_command != NULL)
-	{
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_CONFIG_LONG,
-								   ZCLK_OPTION_MAIN_CONFIG_SHORT, NULL, ZCLK_OPTION_MAIN_CONFIG_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_DEBUG_LONG,
-								   ZCLK_OPTION_MAIN_DEBUG_SHORT, 0, ZCLK_OPTION_MAIN_DEBUG_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_LOG_LEVEL_LONG,
-								   ZCLK_OPTION_MAIN_LOG_LEVEL_SHORT, NULL, ZCLK_OPTION_MAIN_LOG_LEVEL_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLS_LONG,
-								   ZCLK_OPTION_MAIN_TLS_SHORT, 0, ZCLK_OPTION_MAIN_TLS_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSCACERT_LONG,
-								   ZCLK_OPTION_MAIN_TLSCACERT_SHORT, NULL, ZCLK_OPTION_MAIN_TLSCACERT_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSCERT_LONG,
-								   ZCLK_OPTION_MAIN_TLSCERT_SHORT, NULL, ZCLK_OPTION_MAIN_TLSCERT_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSKEY_LONG,
-								   ZCLK_OPTION_MAIN_TLSKEY_SHORT, NULL, ZCLK_OPTION_MAIN_TLSKEY_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSVERIFY_LONG,
-								   ZCLK_OPTION_MAIN_TLSVERIFY_SHORT, 0, ZCLK_OPTION_MAIN_TLSVERIFY_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_INTERACTIVE_LONG,
-								   ZCLK_OPTION_MAIN_INTERACTIVE_SHORT, 0, ZCLK_OPTION_MAIN_INTERACTIVE_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_HOST_LONG,
-								   ZCLK_OPTION_MAIN_HOST_SHORT, NULL, ZCLK_OPTION_MAIN_HOST_DESC);
-		zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_VERSION_LONG,
-								   ZCLK_OPTION_MAIN_VERSION_SHORT, 0, ZCLK_OPTION_MAIN_VERSION_DESC);
+    zclk_command *main_command = new_zclk_command(main_command_name, "cld",
+                                                 "CLD Docker Client",
+                                                 &main_cmd_handler);
+    
+    if (main_command != NULL)
+    {
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_CONFIG_LONG,
+                                   ZCLK_OPTION_MAIN_CONFIG_SHORT, NULL, ZCLK_OPTION_MAIN_CONFIG_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_DEBUG_LONG,
+                                   ZCLK_OPTION_MAIN_DEBUG_SHORT, 0, ZCLK_OPTION_MAIN_DEBUG_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_LOG_LEVEL_LONG,
+                                   ZCLK_OPTION_MAIN_LOG_LEVEL_SHORT, NULL, ZCLK_OPTION_MAIN_LOG_LEVEL_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLS_LONG,
+                                   ZCLK_OPTION_MAIN_TLS_SHORT, 0, ZCLK_OPTION_MAIN_TLS_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSCACERT_LONG,
+                                   ZCLK_OPTION_MAIN_TLSCACERT_SHORT, NULL, ZCLK_OPTION_MAIN_TLSCACERT_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSCERT_LONG,
+                                   ZCLK_OPTION_MAIN_TLSCERT_SHORT, NULL, ZCLK_OPTION_MAIN_TLSCERT_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSKEY_LONG,
+                                   ZCLK_OPTION_MAIN_TLSKEY_SHORT, NULL, ZCLK_OPTION_MAIN_TLSKEY_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_TLSVERIFY_LONG,
+                                   ZCLK_OPTION_MAIN_TLSVERIFY_SHORT, 0, ZCLK_OPTION_MAIN_TLSVERIFY_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_INTERACTIVE_LONG,
+                                   ZCLK_OPTION_MAIN_INTERACTIVE_SHORT, 0, ZCLK_OPTION_MAIN_INTERACTIVE_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_HOST_LONG,
+                                   ZCLK_OPTION_MAIN_HOST_SHORT, NULL, ZCLK_OPTION_MAIN_HOST_DESC);
+        zclk_command_string_option(main_command, ZCLK_OPTION_MAIN_VERSION_LONG,
+                                   ZCLK_OPTION_MAIN_VERSION_SHORT, 0, ZCLK_OPTION_MAIN_VERSION_DESC);
 
-		arraylist_add(main_command->sub_commands, sys_commands());
-		arraylist_add(main_command->sub_commands, ctr_commands());
-		arraylist_add(main_command->sub_commands, img_commands());
-		arraylist_add(main_command->sub_commands, vol_commands());
-		arraylist_add(main_command->sub_commands, net_commands());
-		return main_command;
-	}
-	return NULL;
+        arraylist_add(main_command->sub_commands, sys_commands());
+        arraylist_add(main_command->sub_commands, ctr_commands());
+        arraylist_add(main_command->sub_commands, img_commands());
+        arraylist_add(main_command->sub_commands, vol_commands());
+        arraylist_add(main_command->sub_commands, net_commands());
+        return main_command;
+    }
+    return NULL;
 }
 
 arraylist *create_commands()
 {
-	int err = arraylist_new(&CLI_COMMANDS, (void (*)(void *)) & free_command);
-	if (err == 0)
-	{
-		arraylist_add(CLI_COMMANDS, create_main_command());
-	}
-	else
-	{
-		printf("Error creating commands list");
-	}
-	return CLI_COMMANDS;
+    int err = arraylist_new(&CLI_COMMANDS, (void (*)(void *)) & free_command);
+    if (err == 0)
+    {
+        arraylist_add(CLI_COMMANDS, create_main_command());
+    }
+    else
+    {
+        printf("Error creating commands list");
+    }
+    return CLI_COMMANDS;
 }
 
 int main(int argc, char *argv[])
 {
-	docker_log_set_level(LOG_INFO);
+    docker_log_set_level(LOG_INFO);
 
-	d_err_t res = docker_api_init();
+    d_err_t res = docker_api_init();
 
-	if (res == 0)
-	{
-		if (argc > 0)
-		{
-			docker_log_debug("command name is %s\n", argv[0]);
-			main_command_name = argv[0];
+    if (res == 0)
+    {
+        if (argc > 0)
+        {
+            docker_log_debug("command name is %s\n", argv[0]);
+            main_command_name = argv[0];
 
-			start_lua_interpreter();
+            start_lua_interpreter();
 
 
-			zclk_res err = zclk_command_exec(create_main_command(), &ctx, argc, argv);
+            zclk_res err = zclk_command_exec(create_main_command(), &ctx, argc, argv);
 
-			if (err != ZCLK_RES_SUCCESS)
-			{
-				docker_log_error("Error: invalid command.\n");
-			}
+            if (err != ZCLK_RES_SUCCESS)
+            {
+                docker_log_error("Error: invalid command.\n");
+            }
 
-			stop_lua_interpreter();
-		}
-	}
-	else
-	{
-		printf("Docker API initialization error!\n");
-	}
+            stop_lua_interpreter();
+        }
+    }
+    else
+    {
+        printf("Docker API initialization error!\n");
+    }
 
-	docker_api_cleanup();
+    docker_api_cleanup();
 
-	return 0;
+    return 0;
 }
